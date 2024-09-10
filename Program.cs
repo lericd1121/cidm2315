800daa146f2977c705ca079a10c4ac1824ef48a2
lericd1121 <lericdavid@gmail.com>
2024-09-09 23:19:16 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Homework1;
+class Program
+{
+    static void Main(string[] args)
+    {
+       double X = 2.5;
+       double Y = 3.3;
+      double Z = 4 * Math.Pow(X, 2) + 3 * Y;
+       Console.WriteLine($"x = {X} Y = {Y}") ;
+       Console.WriteLine($"the value of Z is: {Z}");
+    }
+}
